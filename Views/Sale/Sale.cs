34e969308d24117427d9ba84eaafd101b85edd20
Antonyok69 <onyokvelasco2002@gmail.com>
2026-05-19 21:41:22 +0800

    Updated sales order actions

--- Views/Sale/Sale.cs
+++ Views/Sale/Sale.cs
@@ -20,21 +20,31 @@ namespace InventoryApp.InventoryApp.Views
         {
             DataTable dt = orderManager.GetOrders();
             dataGridView1.DataSource = dt;
-            
         }
 
+        // CHECK BUTTON
         private void button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Order checked successfully.", "Sales", MessageBoxButtons.OK, MessageBoxIcon.Information);
             DisplayOrders();
-            MessageBox.Show("Sales orders refreshed.", "Sales", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // REMOVE BUTTON
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+                if (MessageBox.Show("Remove this order?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    orderManager.DeleteOrder(id);
+                    DisplayOrders();
+                }
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
-        {
-            DisplayOrders();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
         {
             DisplayOrders();
         }
