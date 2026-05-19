4f982bf8815791bbff98442560faf5f907030a2f
Antonyok69 <onyokvelasco2002@gmail.com>
2026-05-19 20:43:47 +0800

    Added dashboard summary data

--- Views/Dashboard/Dashboard.cs
+++ Views/Dashboard/Dashboard.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using InventoryApp.Data;
 
 namespace InventoryApp.Views.Dashboard
 {
@@ -7,6 +11,32 @@ namespace InventoryApp.Views.Dashboard
         public Dashboard()
         {
             InitializeComponent();
+            LoadDashboardData();
+        }
+
+        private void LoadDashboardData()
+        {
+            ProductManager productManager = new ProductManager();
+            OrderManager orderManager = new OrderManager();
+
+            DataTable products = productManager.GetProducts();
+            DataTable orders = orderManager.GetOrders();
+
+            label3.Text = products.Rows.Count.ToString();
+
+            decimal totalRevenue = orders.AsEnumerable()
+                .Sum(row => row.Field<decimal>("Total"));
+
+            label2.Text = "$" + totalRevenue.ToString("N0");
+
+            label7.Text = orders.Rows.Count.ToString();
+
+            int totalCategories = products.AsEnumerable()
+                .Select(row => row.Field<string>("category"))
+                .Distinct()
+                .Count();
+
+            label5.Text = totalCategories.ToString();
         }
     }
 }
